48713d0979cd38a3cb0b8fec091d29a815933126
shy <mostima2019@outlook.com>
2024-09-15 16:50:32 +0300

    code

--- B11.c
+++ B11.c
@@ -1,9 +1,25 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int cycleFlag = 0, numFlag = 0, zeroFlag = 0, minusFlag =0;
-    long long sum = 0, symbol, readedSymbol = 0;//2:- 3+
-    
-    printf("%lld\n", sum);
+int main(){
+
+    long num, cnt=0, sum=0; char c;
+
+    while(((c=getchar())!='\n') && (c!=EOF)){
+        if(c=='\n'){continue;}
+        else{ungetc(c,stdin);}
+        if(scanf("%ld",&num)==0){break;}
+        else if(cnt==0){
+            sum = num;
+        }
+        else if(cnt%2==1){
+            sum = sum + num;
+        }
+        else if(cnt%2==0){
+            sum = sum - num;
+        }
+        cnt++;
+    }
+    printf("%ld", sum);
+
     return 0;
 }
