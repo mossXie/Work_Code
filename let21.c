84ceceba044e15e9029d27790c5e46485bcc4f5d
shy <mostima2019@outlook.com>
2024-09-30 00:22:37 +0300

    code

--- let21.c
+++ let21.c
@@ -1,20 +1,19 @@
 /*class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode prehead = new ListNode(-1);
-
-        ListNode prev = prehead;
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                prev.next = l1;
-                l1 = l1.next;
-            } else {
-                prev.next = l2;
-                l2 = l2.next;
+public:
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
+        ListNode dummy{}; // 用哨兵节点简化代码逻辑
+        auto cur = &dummy; // cur 指向新链表的末尾
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                cur->next = list1; // 把 list1 加到新链表中
+                list1 = list1->next;
+            } else { // 注：相等的情况加哪个节点都是可以的
+                cur->next = list2; // 把 list2 加到新链表中
+                list2 = list2->next;
             }
-            prev = prev.next;
+            cur = cur->next;
         }
-        prev.next = l1 == null ? l2 : l1;
-
-        return prehead.next;
+        cur->next = list1 ? list1 : list2; // 拼接剩余链表
+        return dummy.next;
     }
 }*/
