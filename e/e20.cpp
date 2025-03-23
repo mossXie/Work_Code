04d51acd452a9159f46e3d77b4a475a1e7c5191c
Се Цзитао <mostima2019@outlook.com>
2025-03-23 18:27:46 +0300

    change

--- e/e20.cpp
+++ e/e20.cpp
@@ -7,163 +7,165 @@ using namespace std;
 // 物体
 class Figure {
 public:
-    virtual bool canMove(char fromFile, int fromRank, char toFile, int toRank) const = 0;
-    virtual ~Figure() {}
+    virtual bool canMove(char fromName, int fromNum, char toName, int toNum) const = 0;
+    virtual ~Figure();
 };
+
+Figure::~Figure() {}
 
 // 国王
 class King : public Figure {
 public:
-    bool canMove(char fromFile, int fromRank, char toFile, int toRank) const override {
-        int fileDiff = abs(fromFile - toFile);
-        int rankDiff = abs(fromRank - toRank);
-        return (fileDiff <= 1 && rankDiff <= 1 && (fileDiff + rankDiff) >= 1);
-    }
+    bool canMove(char fromName, int fromNum, char toName, int toNum) const override;
 };
+
+bool King::canMove(char fromName, int fromNum, char toName, int toNum) const {
+    int nameDiff = abs(fromName - toName);
+    int numDiff = abs(fromNum - toNum);
+    return (nameDiff <= 1 && numDiff <= 1 && (nameDiff + numDiff) >= 1);
+}
 
 // 皇后
 class Queen : public Figure {
 public:
-    bool canMove(char fromFile, int fromRank, char toFile, int toRank) const override {
-        int fileDiff = abs(fromFile - toFile);
-        int rankDiff = abs(fromRank - toRank);
-        return (fileDiff == rankDiff) || (fileDiff == 0 || rankDiff == 0);
-    }
+    bool canMove(char fromName, int fromNum, char toName, int toNum) const override;
 };
+
+bool Queen::canMove(char fromName, int fromNum, char toName, int toNum) const {
+    int nameDiff = abs(fromName - toName);
+    int numDiff = abs(fromNum - toNum);
+    return (nameDiff == numDiff) || (nameDiff == 0 || numDiff == 0);
+}
 
 // 战车
 class Rook : public Figure {
 public:
-    bool canMove(char fromFile, int fromRank, char toFile, int toRank) const override {
-        return (fromFile == toFile) || (fromRank == toRank);
-    }
+    bool canMove(char fromName, int fromNum, char toName, int toNum) const override;
 };
+
+bool Rook::canMove(char fromName, int fromNum, char toName, int toNum) const {
+    return (fromName == toName) || (fromNum == toNum);
+}
 
 // 主教
 class Bishop : public Figure {
 public:
-    bool canMove(char fromFile, int fromRank, char toFile, int toRank) const override {
-        int fileDiff = abs(fromFile - toFile);
-        int rankDiff = abs(fromRank - toRank);
-        return (fileDiff == rankDiff);
-    }
+    bool canMove(char fromName, int fromNum, char toName, int toNum) const override;
 };
+
+bool Bishop::canMove(char fromName, int fromNum, char toName, int toNum) const {
+    int nameDiff = abs(fromName - toName);
+    int numDiff = abs(fromNum - toNum);
+    return (nameDiff == numDiff);
+}
 
 // 骑士
 class Knight : public Figure {
 public:
-    bool canMove(char fromFile, int fromRank, char toFile, int toRank) const override {
-        int fileDiff = abs(fromFile - toFile);
-        int rankDiff = abs(fromRank - toRank);
-        return ((fileDiff == 1 && rankDiff == 2) || (fileDiff == 2 && rankDiff == 1));
-    }
+    bool canMove(char fromName, int fromNum, char toName, int toNum) const override;
 };
+
+bool Knight::canMove(char fromName, int fromNum, char toName, int toNum) const {
+    int nameDiff = abs(fromName - toName);
+    int numDiff = abs(fromNum - toNum);
+    return ((nameDiff == 1 && numDiff == 2) || (nameDiff == 2 && numDiff == 1));
+}
 
 // 解析器类
 class Parser {
 public:
-    static bool parseInput(const string& input, char& piece, char& fromFile, int& fromRank, char& toFile, int& toRank) {
-        size_t space1 = input.find(' ');
-        size_t space2 = input.find(' ', space1 + 1);
-        
-        if (space1 == string::npos || space2 == string::npos) {
-            return false;
-        }
-        
-        piece = input[0];
-        if (piece >= 'a' && piece <= 'z') {
-            piece = piece - 32;
-        }
-        
-        string fromSquare = input.substr(space1 + 1, space2 - space1 - 1);
-        string toSquare = input.substr(space2 + 1);
-        
-        if (fromSquare.length() != 2 || !isalpha(fromSquare[0]) || !isdigit(fromSquare[1])) {
-            return false;
-        }
-        fromFile = fromSquare[0];
-        if (fromFile >= 'a' && fromFile <= 'z') {
-            fromFile = fromFile - 32;
-        }
-        fromRank = fromSquare[1] - '0';
-        
-        if (toSquare.length() != 2 || !isalpha(toSquare[0]) || !isdigit(toSquare[1])) {
-            return false;
-        }
-        toFile = toSquare[0];
-        if (toFile >= 'a' && toFile <= 'z') {
-            toFile = toFile - 32;
-        }
-        toRank = toSquare[1] - '0';
-        
-        if (fromFile < 'A' || fromFile > 'H' || toFile < 'A' || toFile > 'H' ||
-            fromRank < 1 || fromRank > 8 || toRank < 1 || toRank > 8) {
-            return false;
-        }
-        
-        return true;
-    }
+    static bool parseInput(const string& input, char& piece, char& fromName, int& fromNum, char& toName, int& toNum);
 };
+
+bool Parser::parseInput(const string& input, char& piece, char& fromName, int& fromNum, char& toName, int& toNum) {
+    string fromAddress = input.substr(2, 3); // 起始位置
+    string toAddress = input.substr(6);     // 目标位置
+
+    piece = input[0];
+
+    fromName = fromAddress[0];
+    if (fromName >= 'a' && fromName <= 'z') {
+        fromName = fromName - 32;
+    }
+    fromNum = fromAddress[1] - '0';
+
+    toName = toAddress[0];
+    if (toName >= 'a' && toName <= 'z') {
+        toName = toName - 32;
+    }
+    toNum = toAddress[1] - '0';
+
+    if (fromName < 'A' || fromName > 'H' || toName < 'A' || toName > 'H' ||
+        fromNum < 1 || fromNum > 8 || toNum < 1 || toNum > 8) {
+        return false;
+    }
+
+    return true;
+}
 
 // 主检查器类
 class ChessChecker {
-    private:
-        Figure* figures[5]; 
-    
-    public:
-        ChessChecker() {
-            figures[0] = new King();
-            figures[1] = new Queen();
-            figures[2] = new Rook();
-            figures[3] = new Bishop();
-            figures[4] = new Knight();
-        }
-    
-        ~ChessChecker() {
-            for (int i = 0; i < 5; i++) {
-                delete figures[i];
-            }
-        }
-    
-        bool isValidMove(const string& input) {
-            char piece, fromFile, toFile;
-            int fromRank, toRank;
-            
-            if (!Parser::parseInput(input, piece, fromFile, fromRank, toFile, toRank)) {
-                return false;
-            }
-            
-            Figure* fig = nullptr;
-            switch (piece) {
-                case 'K':
-                    fig = figures[0];
-                    break;
-                case 'Q':
-                    fig = figures[1];
-                    break;
-                case 'R':
-                    fig = figures[2];
-                    break;
-                case 'B':
-                    fig = figures[3];
-                    break;
-                case 'N':
-                    fig = figures[4];
-                    break;
-                default:
-                    return false;
-            }
-            
-            return fig->canMove(fromFile, fromRank, toFile, toRank);
-        }
+private:
+    Figure* figures[5];
+
+public:
+    ChessChecker();
+    ~ChessChecker();
+    bool isValidMove(const string& input);
 };
+
+ChessChecker::ChessChecker() {
+    figures[0] = new King();
+    figures[1] = new Queen();
+    figures[2] = new Rook();
+    figures[3] = new Bishop();
+    figures[4] = new Knight();
+}
+
+ChessChecker::~ChessChecker() {
+    for (int i = 0; i < 5; i++) {
+        delete figures[i];
+    }
+}
+
+bool ChessChecker::isValidMove(const string& input) {
+    char piece, fromName, toName;
+    int fromNum, toNum;
+
+    if (!Parser::parseInput(input, piece, fromName, fromNum, toName, toNum)) {
+        return false;
+    }
+
+    Figure* fig = nullptr;
+    switch (piece) {
+        case 'K':
+            fig = figures[0];
+            break;
+        case 'Q':
+            fig = figures[1];
+            break;
+        case 'R':
+            fig = figures[2];
+            break;
+        case 'B':
+            fig = figures[3];
+            break;
+        case 'N':
+            fig = figures[4];
+            break;
+        default:
+            return false;
+    }
+
+    return fig->canMove(fromName, fromNum, toName, toNum);
+}
 
 int main() {
     string input;
     getline(cin, input);
-    
+
     ChessChecker checker;
     cout << (checker.isValidMove(input) ? "YES" : "NO") << endl;
-    
+
     return 0;
 }
--- e/e9.cpp
+++ e/e9.cpp
@@ -1,65 +1,95 @@
 #include <iostream>
+
 using namespace std;
 
-class Vector_2d{
-	public:
-		Vector_2d(double x = 0.,double y = 0.){
-			x_ = x;
-			y_ = y;
-		}
-		void print(const char*str)const;
-		Vector_2d operator+(const Vector_2d & op) const;
-		Vector_2d operator-(const Vector_2d & op) const{
-			return Vector_2d(x_ - op.x_,y_ - op.y_);//simple way
-		}
-		const Vector_2d& operator=(const Vector_2d & op);//fast,because don't need to do 2 copy
-		const Vector_2d& operator+=(const Vector_2d& op);
-		Vector_2d operator*(double a) const;
-		friend Vector_2d operator*(double a,const Vector_2d & op);//внешняя функций
-		Vector_2d& operator++();
-		Vector_2d operator++(int);
-		friend ostream& operator<<(ostream& os, const Vector_2d& vec);
-	private:
-		double x_,y_;
+class Vector_2d {
+public:
+    Vector_2d(double x = 0., double y = 0.) {
+        x_ = x;
+        y_ = y;
+    }
+
+    void print(const char *str) const;
+
+    Vector_2d operator+(const Vector_2d &op) const;
+
+    Vector_2d operator-(const Vector_2d &op) const {
+        return Vector_2d(x_ - op.x_, y_ - op.y_);
+    }
+
+    const Vector_2d &operator=(const Vector_2d &op);
+    const Vector_2d &operator+=(const Vector_2d &op);
+
+    const Vector_2d &operator++();
+    Vector_2d operator++(int);
+    Vector_2d operator*(double a) const;
+
+    double &operator[](int index);
+    friend Vector_2d operator*(double a, const Vector_2d &op);
+    friend ostream &operator<<(ostream &os, const Vector_2d &op);
+
+    friend istream &operator>>(istream &is, Vector_2d &op);
+
+private:
+    double x_, y_;
 };
-const Vector_2d& Vector_2d::operator=(const Vector_2d & op){
-	x_ =op.x_;
-	y_ =op.y_;
-	return *this;
+
+Vector_2d Vector_2d::operator++(int) {
+    Vector_2d tmp(x_, y_);
+    x_ = x_ + 1;
+    y_ = y_ + 1;
+    return tmp;
 }
-Vector_2d Vector_2d::operator+(const Vector_2d & op) const{
-	Vector_2d tmp;
-	tmp.x_ = x_ + op.x_;
-	tmp.y_ = y_ + op.y_;
-	return tmp;
+
+const Vector_2d &Vector_2d::operator++() {
+    x_ = x_ + 1;
+    y_ = y_ + 1;
+    return *this;
 }
-const Vector_2d& Vector_2d::operator+=(const Vector_2d & op){
-	x_ += op.x_;
-	y_ += op.y_;
-	return *this;
+
+istream &operator>>(istream &is, Vector_2d &op) {
+    is >> op.x_ >> op.y_;
+    return is;
 }
-Vector_2d operator*(double a,const Vector_2d & op){
-	return Vector_2d(op.x_*a,op.y_*a);
-}
-Vector_2d Vector_2d::operator*(double a) const{
-	return Vector_2d(x_*a,y_*a);
-}
-void Vector_2d::print(const char*str = 0)const{
-	if(str) cout << str << " = ";
-	cout << "(" << x_ << "," << y_ << ")" << endl;
-}
-Vector_2d& Vector_2d::operator++() {
-	x_ += 1;
-	y_ += 1;
-	return *this;
-}
-Vector_2d Vector_2d:: operator++(int) {
-	Vector_2d temp = *this;
-	x_ += 1;
-	y_ += 1;
-	return temp;
-}
-ostream& operator<<(ostream& os, const Vector_2d& vec) {
-    os << "(" << vec.x_ << ", " << vec.y_ << ")";
+
+ostream &operator<<(ostream &os, const Vector_2d &op) {
+    os << "(" << op.x_ << "," << op.y_ << ")";
     return os;
+}
+
+double &Vector_2d::operator[](int index) {
+    if (index == 0) return x_;
+    else return y_;
+}
+
+const Vector_2d &Vector_2d::operator=(const Vector_2d &op) {
+    x_ = op.x_;
+    y_ = op.y_;
+    return *this;
+}
+
+Vector_2d Vector_2d::operator+(const Vector_2d &op) const {
+    Vector_2d tmp;
+    tmp.x_ = x_ + op.x_;
+    tmp.y_ = y_ + op.y_;
+    return tmp;
+}
+
+const Vector_2d &Vector_2d::operator+=(const Vector_2d &op) {
+    x_ += op.x_;
+    y_ += op.y_;
+    return *this;
+}
+
+Vector_2d operator*(double a, const Vector_2d &op) {
+    return Vector_2d(op.x_ * a, op.y_ * a);
+}
+
+Vector_2d Vector_2d::operator*(double a) const {
+    return Vector_2d(x_ * a, y_ * a);
+}
+
+void Vector_2d::print(const char *str = 0) const {
+    if (str) cout << str << " = ";
+    cout << "(" << x_ << "," << y_ << ")" << endl;
 }
